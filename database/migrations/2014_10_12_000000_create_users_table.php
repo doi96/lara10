29c78635fbf241101b43a2e4f5061b6909d8d08e
doi96 <thanhdoi.it96@hotmail.com>
2023-12-16 12:06:03 +0700

    add login admin function

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            // $table->string('fullname',50);
+            $table->string('phone',20)->nullable();
+            $table->string('province_id',10)->nullable();
+            $table->string('district_id',10)->nullable();
+            $table->string('ward_id',10)->nullable();
+            $table->string('address')->nullable();
+            $table->dateTime('birthdate')->nullable();
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+            $table->text('user_agent')->nullable();
+            $table->text('ip')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Backend\AuthController;
+use App\Http\Controllers\Backend\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* BACKEND ROUTE*/
+Route::get('dashboard/index', [DashboardController::class, 'index']) -> name('dashboard.index');
+
+Route::get('admin', [AuthController::class, 'index']) -> name('auth.admin');
+Route::post('login', [AuthController::class, 'login']) -> name('auth.login');
